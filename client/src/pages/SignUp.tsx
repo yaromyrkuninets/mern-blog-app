271ae87b28ea5ab6e7ac28f24b53908d778b1d4f
yaromyrkuninets <kuninets.y@gmail.com>
2024-01-24 21:02:47 +0200

    add functionality to the sign up page

--- client/src/pages/SignUp.tsx
+++ client/src/pages/SignUp.tsx
@@ -1,7 +1,63 @@
-import { Button, Label, TextInput } from "flowbite-react"
-import { Link } from "react-router-dom"
+import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react"
+import { useState, FormEvent } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+interface FormData {
+    username?: string;
+    email?: string;
+    password?: string;
+}
 
 const SignUp = () => {
+
+    const [formData, setFormData] = useState<FormData>({});
+    const [errorMessage, setErrorMessage] = useState<string | null>(null);
+    const [loading, setLoading] = useState(false);
+    const navigate = useNavigate();
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+        setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
+    };
+
+    const handleSubmit = async (e: FormEvent) => {
+        e.preventDefault();
+
+        if (!formData.username || !formData.email || !formData.password) {
+            return setErrorMessage('Please fill out all fields.');
+        }
+
+        try {
+            setLoading(true);
+            setErrorMessage(null);
+
+            const res = await fetch('/api/auth/signup', {
+                method: 'POST',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify(formData)
+            });
+
+            const data = await res.json();
+
+            if (data.success === false) {
+                return setErrorMessage(data.message)
+            }
+
+            setLoading(false);
+
+            if (res.ok) {
+                navigate('/sign-in')
+            }
+        } catch (error) {
+            if (error instanceof Error) {
+                setErrorMessage(error.message);
+            } else {
+                setErrorMessage('An unknown error occurred');
+            }
+            setLoading(false);
+        }
+    }
+      
+
     return (
         <div className='min-h-screen mt-20'>
             <div className='flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5'>
@@ -20,13 +76,14 @@ const SignUp = () => {
                 </div>
                 {/* right */}
                 <div className="flex-1">
-                    <form className='flex flex-col gap-4' action="">
+                    <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
                         <div className="">
                             <Label value='Your username' />
                             <TextInput
                                 type='text'
                                 placeholder='Username'
                                 id='username'
+                                onChange={handleChange}
                             />
                         </div>
 
@@ -36,6 +93,7 @@ const SignUp = () => {
                                 type='email'
                                 placeholder='name@company.com'
                                 id='email'
+                                onChange={handleChange}
                             />
                         </div>
 
@@ -45,14 +103,23 @@ const SignUp = () => {
                                 type='password'
                                 placeholder='Password'
                                 id='password'
+                                onChange={handleChange}
                             />
                         </div>
 
                         <Button
                             gradientDuoTone='purpleToPink'
                             type='submit'
+                            disabled={loading}
                         >
-                            Sign Up
+                            {
+                                loading ? (
+                                    <>
+                                        <Spinner size='sm' />
+                                        <span className="pl-3">Loading...</span>
+                                    </>
+                                ) : 'Sign Up'
+                            }
                         </Button>
                     </form>
 
@@ -62,6 +129,14 @@ const SignUp = () => {
                             Sign In
                         </Link>
                     </div>
+
+                    {
+                        errorMessage && (
+                            <Alert className='mt-5' color='failure'>
+                                {errorMessage}
+                            </Alert>
+                        )
+                    }
                 </div>
             </div>
         </div>

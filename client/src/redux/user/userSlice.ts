18d4862d9b46ea624b792dc8e0f4a8e8c0065965
yaromyrkuninets <kuninets.y@gmail.com>
2024-01-28 17:26:27 +0200

    complete delete user account functionality

--- client/src/redux/user/userSlice.ts
+++ client/src/redux/user/userSlice.ts
@@ -41,10 +41,33 @@ const userSlice = createSlice({
         updateFailure: (state, action: PayloadAction<string>) => {
             state.loading = false;
             state.error = action.payload;
-        }
+        },
+        deleteUserStart: (state) => {
+            state.loading = true;
+            state.error = null;
+        },
+        deleteUserSuccess: (state) => {
+            state.currentUser = null;
+            state.loading = false;
+            state.error = null;
+        },
+        deleteUserFailure: (state, action) => {
+            state.loading = false;
+            state.error = action.payload;
+        },
     }
 })
 
-export const { signInStart, signInSuccess, signInFailure, updateFailure, updateStart, updateSuccess } = userSlice.actions
+export const { 
+    signInStart, 
+    signInSuccess, 
+    signInFailure, 
+    updateFailure, 
+    updateStart, 
+    updateSuccess,
+    deleteUserFailure,
+    deleteUserStart,
+    deleteUserSuccess 
+} = userSlice.actions
 
 export default userSlice.reducer;

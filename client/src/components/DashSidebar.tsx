eaf9edefeee46a15db7ab5fa2a8c0ee076edfeb6
yaromyrkuninets <kuninets.y@gmail.com>
2024-02-02 17:55:37 +0200

    add dashboard overview to the admin dashboard

--- client/src/components/DashSidebar.tsx
+++ client/src/components/DashSidebar.tsx
@@ -69,6 +69,21 @@ const DashSidebar = () => {
         <Sidebar className='w-full md:w-56'>
             <Sidebar.Items>
                 <Sidebar.ItemGroup className='flex flex-col gap-1'>
+
+                    {
+                        currentUser && currentUser.isAdmin && (
+                            <Link to='/dashboard?tab=dash'>
+                                <Sidebar.Item
+                                    active = {tab === 'dash' || !tab}
+                                    icon={HiChartPie}
+                                    as='div'
+                                >
+                                    Dashboard
+                                </Sidebar.Item>
+                            </Link>
+                        )
+                    }
+
                     <Link to='/dashboard?tab=profile'>
                         <Sidebar.Item active={tab === 'profile'} icon={HiUser} label={currentUser.isAdmin ? 'Admin' : 'User'} labelColor='dark' as='div'>
                             Profile
